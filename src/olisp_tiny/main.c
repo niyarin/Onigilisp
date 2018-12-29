9212180b2c8115599dba1390480dfbe7bcbc795c
niyarin <inoanet@gmail.com>
2018-12-29 21:55:33 +0900

    Update main.c

--- src/olisp_tiny/main.c
+++ src/olisp_tiny/main.c
@@ -5,7 +5,7 @@
 #include "ebm_frontend.h"
 #include "olisp_cinterface.h"
 
-void main_load_1script(char *filename){
+uintptr_t main_load_1script(char *filename){
     FILE *fp;
     if ((fp = fopen(filename,"r")) == NULL){
         uintptr_t error_port =  EBM_frontend_allocate_output_file_port_CA(stderr,EBM_malloc_wrapper,EBM_NULL);
@@ -25,15 +25,23 @@ void main_load_1script(char *filename){
     OLISP_state olisp_state;
     olisp_state.allocator = allocator;olisp_state.allocator_env = allocator_env;
 
-    uintptr_t res = OLISP_cfun_call(&olisp_state,OLISP_read,1,port);
+    uintptr_t res = EBM_NULL;
+    while (1){
+        uintptr_t read_obj = OLISP_cfun_call(&olisp_state,OLISP_read,1,port);
+        res = EBM_allocate_pair(read_obj,res,allocator,allocator_env);
+        if (read_obj == EBM_EOF){
+            break;
+        }
+    }
+    return res;
 }
 
 int main(int argc,char **argv){
     if (argc == 1){
-    
+
     }else if (argc == 2){
         char *filename = argv[1];
-        main_load_1script(filename);
+        uintptr_t code = main_load_1script(filename);
     }
     return 0;
 }
