ae0f5d2f5c645019d60327d882b3e3a8c27e3a17
niyarin <inoanet@gmail.com>
2019-03-21 15:58:46 +0900

    Suuport for non ascii character in symbol_trie

--- src/ebm_symbol_trie.c
+++ src/ebm_symbol_trie.c
@@ -5,6 +5,7 @@
 
 #define EBM_SYMBOL_TRIE_INDEX_POSITION_TABLE 3
 #define EBM_SYMBOL_TRIE_INDEX_CHECK_TABLE 4
+#define EBM_SYMBOL_TRIE_INDEX_ITEMS 5
 #define EBM_SYMBOL_TRIE_INDEX_LARGE_INDEX_TABLE 7
 #define EBM_SYMBOL_TRIE_INDEX_LARGE_INDEX_TABLE_SIZE 8
 
@@ -18,7 +19,7 @@ uintptr_t EBM_allocate_symbol_trie(EBM_ALLOCATOR allocator,uintptr_t allocator_e
     EBM_record_primitive_set_CA(res,1,mem_size);
     EBM_record_primitive_set_CA(res,2,sizeof(uint16_t));
     int i,j,_i;
-    int[2] position_and_check_table = {EBM_SYMBOL_TRIE_INDEX_POSITION_TABLE,EBM_SYMBOL_TRIE_INDEX_CHECK_TABLE};
+    int position_and_check_table[2] = {EBM_SYMBOL_TRIE_INDEX_POSITION_TABLE,EBM_SYMBOL_TRIE_INDEX_CHECK_TABLE};
     for (_i=0;_i<2;_i++){
         int i=position_and_check_table[_i];
         uint16_t *mem = (uint16_t*)allocator(sizeof(uint16_t) * mem_size,allocator_env);
@@ -35,7 +36,7 @@ uintptr_t EBM_allocate_symbol_trie(EBM_ALLOCATOR allocator,uintptr_t allocator_e
     }
 
     uintptr_t *mem = (uintptr_t*)allocator(sizeof(uintptr_t) * mem_size,allocator_env);
-    EBM_record_primitive_set_CA(res,5,(uintptr_t)mem);
+    EBM_record_primitive_set_CA(res,EBM_SYMBOL_TRIE_INDEX_ITEMS,(uintptr_t)mem);
     for (i=0;i<mem_size;i++){
         mem[i] = EBM_UNDEF;
     }
@@ -47,17 +48,34 @@ uintptr_t EBM_allocate_symbol_trie(EBM_ALLOCATOR allocator,uintptr_t allocator_e
     return res;
 }
 
+uintptr_t EBM_trie_char_index(uintptr_t index,uintptr_t character_table){
+    if (index < 128){
+        return index+1;
+    }
+    
+    uintptr_t fx_index_apair = EBM_char_table_ref_CA(character_table,index);
+    if (fx_index_apair){
+       return EBM_CDR(fx_index_apair);
+    }
+    
+    return 0;
+}
 
 uintptr_t EBM_symbol_trie_ref(uintptr_t trie,uintptr_t symbol){
-    uintptr_t mem_size = EBM_record_ref(trie,1);
-    uint32_t *symbol_data = (uint32_t*)EBM_record_ref(symbol,2);
-    uint16_t *base = (uint16_t*)EBM_record_ref(trie,EBM_SYMBOL_TRIE_INDEX_POSITION_TABLE);
-    uint16_t *check = (uint16_t*)EBM_record_ref(trie,EBM_SYMBOL_TRIE_INDEX_CHECK_TABLE);
-    uintptr_t *items = (uintptr_t*)EBM_record_ref(trie,5);
+    uintptr_t mem_size = EBM_record_ref_CA(trie,1);
+    uint32_t *symbol_data = (uint32_t*)EBM_record_ref_CA(symbol,2);
+    uint16_t *base = (uint16_t*)EBM_record_ref_CA(trie,EBM_SYMBOL_TRIE_INDEX_POSITION_TABLE);
+    uint16_t *check = (uint16_t*)EBM_record_ref_CA(trie,EBM_SYMBOL_TRIE_INDEX_CHECK_TABLE);
+    uintptr_t *items = (uintptr_t*)EBM_record_ref_CA(trie,EBM_SYMBOL_TRIE_INDEX_ITEMS);
     uintptr_t index = 0;
     uintptr_t pos = 1;
     do {
-        uintptr_t new_pos = base[pos] + EBM_char_index(symbol_data[index],0);
+        uintptr_t symbol_index = EBM_trie_char_index(symbol_data[index],0);
+        if (symbol_index == 0){
+            return EBM_UNDEF;
+        }
+        
+        uintptr_t new_pos = base[pos] + symbol_index;
         if (check[new_pos] != pos){
             return EBM_UNDEF;
         }
