9801ca914c8164a9051957b6440811ea5af16abe
niyarin <inoanet@gmail.com>
2019-04-17 00:41:50 +0900

    Add libraries for olisp-tiny.

---
+++ lib/olisp_tiny/olisp_tiny_library.scm
@@ -0,0 +1,23 @@
+(define-library (olisp base-library1)
+   (export caar cadr cdar cddr)
+
+   (define caar
+     (lambda (x) (car (car x))))
+
+   (define cadr 
+     (lambda (x) (car (cdr x))))
+
+   (define cdar 
+     (lambda (x) (cdr (car x))))
+
+   (define cddr
+     (lambda (x) (cdr (cdr x))))
+
+   (define not
+     (lambda (x)
+       (if (eq? x #f)
+         #t
+         #f))))
+
+(import (lisp base-library1))
+
