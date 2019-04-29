25dc5e840d70a998c71f67161c824f1aec33ff29
niyarin <inoanet@gmail.com>
2019-04-29 14:23:22 +0900

    Add assq and rassqs.

--- include/ebm_aux.h
+++ include/ebm_aux.h
@@ -3,6 +3,7 @@
 
 #include "ebm.h"
 
-
+uintptr_t EBM_assq(uintptr_t object,uintptr_t alist);
+uintptr_t EBM_rassq(uintptr_t object,uintptr_t alist);
 #endif
 
--- src/ebm_aux.c
+++ src/ebm_aux.c
@@ -1,1 +1,22 @@
 #include "ebm_aux.h"
+#include "ebm.h"
+
+uintptr_t EBM_assq(uintptr_t object,uintptr_t alist){
+    while (alist != EBM_NULL){
+        if (EBM_eq(EBM_CAAR(alist),object)){
+            return EBM_CAR(alist);
+        }
+        alist = EBM_CDR(alist);
+    }
+    return EBM_FALSE;
+}
+
+uintptr_t EBM_rassq(uintptr_t object,uintptr_t alist){
+    while (alist != EBM_NULL){
+        if (EBM_eq(EBM_CDAR(alist),object)){
+            return EBM_CAR(alist);
+        }
+        alist = EBM_CDR(alist);
+    }
+    return EBM_FALSE;
+}
