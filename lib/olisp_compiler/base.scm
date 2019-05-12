8ddb0a11594648edebf7611909d4b2424ccf77bf
niyarin <inoanet@gmail.com>
2019-05-12 23:28:02 +0900

    Add gen-my-sym.

--- lib/olisp_compiler/base.scm
+++ lib/olisp_compiler/base.scm
@@ -3,6 +3,7 @@
 (define-library (olisp-compiler base)
    (import 
      (olisp-tiny base-library0)
+     (olisp-tiny base-library2)
      (olisp-tiny base-library3)
      )
    (export gen-my-sym my-sym?)
@@ -13,5 +14,9 @@
        my-sym?
        (id my-sym-ref))
 
-
+     (define gen-my-sym
+       (let ((id-number 0))
+         (lambda ()
+               (set! id-number (fx+ id-number 1))
+               (internal-gen-my-sym id-number))))
      ))
